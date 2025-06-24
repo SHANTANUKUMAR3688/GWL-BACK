a432366048dd243751d05b74bcab70b7415e7231
SHANTANUKUMAR3688 <157882524+SHANTANUKUMAR3688@users.noreply.github.com>
2025-06-24 20:21:51 +0530

    first commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 
 // cors
 app.use(cors({
-	origin:"http://localhost:3000",
+	origin:["http://localhost:3000","https://gwl-front-1zw3.vercel.app"],
 	methods:["GET","POST","PUT","DELETE","PATCH"],
 	allowedHeaders:["Content-Type","Authorization"],
 	credentials:true,
@@ -44,7 +44,6 @@ app.get("/", (req, res) => {
 // Routes
 app.use(errorMiddleware);
 app.use('/api', userRoutes);
-
 
 // multer 
 const storage = multer.diskStorage({
