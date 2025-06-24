870230fe174400e7a031e207bffa2988596ea881
SHANTANUKUMAR3688 <157882524+SHANTANUKUMAR3688@users.noreply.github.com>
2025-06-24 20:50:05 +0530

    bcry

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const Admin = require("../models/admin-model");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
